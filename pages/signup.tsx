70d6fba8b9b05901c057c0408fbbe27c4217d044
milovangudelj <milovan.gudelj@gmail.com>
2022-08-19 22:13:16 +0200

    SEO stuff

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,8 +1,17 @@
+import Head from "next/head";
 import { AuthForm, AuthLayout } from "../components/layouts";
 
 const Signup = () => {
 	return (
 		<AuthLayout>
+			<Head>
+				<title>Sign Up | doyourthing</title>
+				<meta
+					name="description"
+					content="Manager web app for doyourthing configurations"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<AuthForm view="signup" />
 		</AuthLayout>
 	);
