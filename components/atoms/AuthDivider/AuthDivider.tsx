6e8ff03ee30fad7a64ea850fea85246237a5871f
milovangudelj <milovan.gudelj@gmail.com>
2022-08-28 23:35:45 +0200

    English or

--- components/atoms/AuthDivider/AuthDivider.tsx
+++ components/atoms/AuthDivider/AuthDivider.tsx
@@ -3,7 +3,7 @@ export const AuthDivider = () => {
 		<div className="w-full flex my-8 items-center">
 			<div className="h-px w-full bg-zinc-300 dark:bg-zinc-700"></div>
 			<span className="text-sm inline-block flex-none dark:text-light-le text-dark-le mx-2">
-				oppure
+				or
 			</span>
 			<div className="h-px w-full bg-zinc-300 dark:bg-zinc-700"></div>
 		</div>
