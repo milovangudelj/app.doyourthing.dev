5a02b357c56ba8d0a109a0f3ad587243fa386a6c
milovangudelj <milovan.gudelj@gmail.com>
2022-09-03 23:53:45 +0200

    Auth redirection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
-import type { NextPage } from 'next'
+import type { GetServerSidePropsContext, NextPage } from "next";
 import Head from "next/head";
 
 import { Layout } from "../components/layouts";
 
-const Home: NextPage = () => {
+import { supabase } from "../lib/supabase";
+
+export const getServerSideProps = async ({
+	req,
+}: GetServerSidePropsContext) => {
+	const { user } = await supabase.auth.api.getUserByCookie(req);
+
+	if (!user) {
+		// If no user, redirect to index.
+		return {
+			props: {},
+			redirect: { destination: "/signin", permanent: false },
+		};
+	}
+
+	// If there is a user, return it.
+	return { props: { user } };
+};
+
+const Home: NextPage = (props: any) => {
 	return (
 		<Layout>
 			<Head>
@@ -15,11 +34,11 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className="flex items-center justify-center min-h-screen">
+			<main className="min-h-screen flex items-center justify-center">
 				<h1>Exciting times ahead!</h1>
 			</main>
 		</Layout>
 	);
 };
 
-export default Home
+export default Home;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,26 @@
+import { GetServerSidePropsContext } from "next";
 import Head from "next/head";
+
+import { supabase } from "../lib/supabase";
+
 import { AuthForm, AuthLayout } from "../components/layouts";
+
+export const getServerSideProps = async ({
+	req,
+}: GetServerSidePropsContext) => {
+	const { user } = await supabase.auth.api.getUserByCookie(req);
+
+	if (user) {
+		// If there is a user, redirect to index and pass it.
+		return {
+			props: { user },
+			redirect: { destination: "/", permanent: false },
+		};
+	}
+
+	// If there no user, do nothing.
+	return { props: {} };
+};
 
 const Signup = () => {
 	return (
