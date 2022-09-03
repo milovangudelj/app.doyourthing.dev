6c8edb8a0b43d4903cc93682a7273a1d41708b56
milovangudelj <milovan.gudelj@gmail.com>
2022-09-03 23:50:50 +0200

    New side menu

--- components/organisms/SideMenu/SideMenu.tsx
+++ components/organisms/SideMenu/SideMenu.tsx
@@ -1,14 +1,16 @@
 import { Logo } from "@doyourthing/ui";
 
+import { Account } from "../../molecules";
+
 export const SideMenu = () => {
 	return (
-		<aside>
-			<Logo />
+		<aside className="min-h-screen h-full relative w-[300px] px-4 py-8">
+			<Logo size="md" type="short" />
 			{/* <SearchBar /> */}
 			{/* <SideNav /> */}
 			{/* <AdSquare /> */}
 			{/* <Divider /> */}
-			{/* <Accuont /> */}
+			<Account />
 		</aside>
 	);
 };
