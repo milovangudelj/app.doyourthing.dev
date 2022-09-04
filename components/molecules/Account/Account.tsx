e6abc08aeb951481eec9e84d721eebf4d9d205ac
milovangudelj <milovan.gudelj@gmail.com>
2022-09-05 00:33:13 +0200

    Using `IconButton`

--- components/molecules/Account/Account.tsx
+++ components/molecules/Account/Account.tsx
@@ -1,6 +1,6 @@
 import { SignOut } from "phosphor-react";
 
-import { Button } from "@doyourthing/ui";
+import { Button, IconButton } from "@doyourthing/ui";
 
 import { useAuth } from "../../../lib/auth";
 
@@ -8,7 +8,7 @@ export const Account = () => {
 	const auth = useAuth();
 
 	return (
-		<div className="sticky w-full top-full flex justify-between items-center border-t border-zinc-600 pt-6 px-2 space-x-2">
+		<div className="w-full flex justify-between items-center border-t border-zinc-600 pt-6 px-2 space-x-2">
 			<div className="w-8 h-8 bg-zinc-700 rounded-full"></div>
 			<div className="flex flex-col flex-1">
 				<span className="text-light-he text-sm font-medium leading-5">
@@ -18,14 +18,13 @@ export const Account = () => {
 					@{auth?.user?.username ?? "helloexample"}
 				</span>
 			</div>
-			<Button
+			<IconButton
 				color="accent"
 				size="sm"
 				onClick={auth?.signOut}
 				variant="text"
-			>
-				<SignOut className="w-5 h-5" width={20} height={20} weight="bold" />
-			</Button>
+				icon={SignOut}
+			/>
 		</div>
 	);
 };
